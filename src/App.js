1cdb37043f4e6628365307c470f91c07c63afc1a
Jay Shrivastava <jay13102000@gmail.com>
2020-05-07 01:49:11 +0530

    Assignment 1 DishdetailComponent

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar,NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
-import './App.css';
 import { DISHES } from './shared/dishes';
 
 class App extends Component {
---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,74 @@
+import React,{ Component } from "react";
+import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+
+
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+                <div className='col-12 col-md-5 m-1'>
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+            )
+        }
+        else {
+            return (<div></div>)
+        }
+    }
+
+    renderComments(comments) {
+        if (comments == null) {
+            return (<div></div>)
+        }
+        const eachComment = comments.map(comment => {
+            return (
+                <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author},
+                    &nbsp;
+                    {new Intl.DateTimeFormat('en-US', {
+                            day: '2-digit',
+                            month: 'short',
+                            year: 'numeric'
+                        }).format(new Date(comment.date))}
+                    </p>
+                </li>
+            )
+        })
+        return (
+            <div className='col-12 col-md-5 m-1'>
+                <h4> Comments </h4>
+                <div className='list-unstyled'>
+                    {eachComment}
+                </div>
+
+            </div>
+        )
+    }
+
+
+
+    render() {
+        const dish = this.props.dish;
+        if (dish == null) {
+            return (<div></div>)
+        }
+       
+        return (
+            <div className='row'>
+                {this.renderDish(dish)}
+                {this.renderComments(dish.comments)}
+            </div>
+        )
+    }
+}
+
+
+export default DishDetail;
